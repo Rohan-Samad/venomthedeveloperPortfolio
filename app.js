eb9e95ed54adc97e8fc62b6fdc3fbfddee952496
Rohan Samad <maidrgayi@gmail.com>
2022-12-07 08:27:31 +0500

    Testing Changes

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.reslove(__dirname, "./portfolio/boild")));
 app.use("/api", require("./Handlers/EmailHandler"));
+app.get("/testing", (req, res) => {
+  res
+    .status(200)
+    .json({ status: "success", message: "Message has been sent successfully" });
+});
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./portfolio/build", "indx.html"));
 });
